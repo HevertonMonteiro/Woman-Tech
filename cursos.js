37ec8cc68079c02a3307d6238ea5551b4c48b0fb
HevertonMonteiro <167649076+HevertonMonteiro@users.noreply.github.com>
2024-10-27 23:36:22 -0300

    atualização do front

--- cursos.js
+++
@@ -1,7 +0,0 @@
-function changeLanguage(lang) {
-    if (lang === 'pt') {
-        alert('Idioma alterado para Português!');
-    } else if (lang === 'en') {
-        alert('Language switched to English!');
-    }
-}
